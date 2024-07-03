bc5e4e915dc1efd7cf55470af7af71944a41561c
Jeongsunga <jsa3341@gmail.com>
2024-07-04 00:52:56 +0900

    fourth video done

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 // + 버튼을 누르면, 할 일이 추가
 // delete 버튼을 누르면, 할 일이 삭제
 // check 버튼을 누르면, 할 일이 끝나면서 밑줄
+// 1. check 버튼을 클릭하는 순간 isComplete를 false에서 true
+// 2. true이면 끝난 걸로 간주하고 밑줄
+//    false이면 안 끝난 걸로 간주하고 그대로
 // 진행중 또는 완료 탭을 누르면, 언더바가 이동
 // 완료 탭은 끝난 아이템만, 진행중 탭은 진행 중인 아이템만 보임
 // 전체 탭을 누르면 다시 전체 아이템으로 돌아옴 
@@ -12,21 +15,62 @@ let taskList = []
 addButton.addEventListener("click", addTask)
 
 function addTask(){
-    let taskContent = taskInput.value
-    taskList.push(taskContent)
+    let task = {
+        id : randomIDGenerate(),
+        taskContent : taskInput.value,
+        isComplete : false
+    }
+    taskList.push(task)
+    console.log(taskList)
     render()
 }
 
 function render(){
     let resultHTML = ''
     for(let i = 0; i < taskList.length; i++){
-        resultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>
+        if(taskList[i].isComplete == true){
+            resultHTML += `<div class="task" style="background-color: gainsboro;">
+                    <div class="task-done">${taskList[i].taskContent}</div>
                     <div>
-                        <button>Check</button>
-                        <button>Delete</button>
+                        <i class="fa-solid fa-rotate-left" onclick="toggleComplete('${taskList[i].id}')" style="margin-right:10px; color:gray;"></i>
+                        <i class="fa-solid fa-trash-can" onclick="deleteTask('${taskList[i].id}')" style="color:red"></i>
                     </div>
                 </div>`
+        } else{
+            resultHTML += `<div class="task">
+                    <div>${taskList[i].taskContent}</div>
+                    <div>
+                        <i class="fa-solid fa-check" onclick="toggleComplete('${taskList[i].id}')" style="margin-right:10px; color:green"></i>
+                        <i class="fa-solid fa-trash-can" onclick="deleteTask('${taskList[i].id}')" style="color:red"></i>
+                    </div>
+                </div>`
+        }
+        
     }
     document.getElementById("task-board").innerHTML = resultHTML
+}
+
+function toggleComplete(id){
+    for(let i = 0; i < taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function deleteTask(id){
+    for(let i = 0; i < taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList.splice(i, 1);
+            break;
+        }
+    }
+    render();
 }
